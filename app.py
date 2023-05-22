fb3be2163fa0316b40fd8c5bcd06dc65ef3a87d0
harrison <851596652@qq.com>
2023-05-22 20:35:57 +0800

    flask _json fix unicode -> chinese

--- app.py
+++ app.py
@@ -1,25 +1,61 @@
+from datetime import datetime, date
+
 from flask import Flask
+# from flask.json import jsonify, dumps
+from flask.json.provider import DefaultJSONProvider
 from markupsafe import escape
 from db.index import read_table
+import json
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        print('MyEncoder: ', obj, type(obj))
+        if isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        if isinstance(obj, datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        if isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return super().default(obj)
+
+        # else:
+        #     return json.JSONEncoder.default(self, obj)
+
 
 print(__name__)
 app = Flask(__name__)
+# app.json = MyEncoder(app)
+
+
+# app.config['JSON_AS_ASCII'] = False
+# app.config['PORT'] = 6000
+
+
+def _json(data):
+    json_str = json.dumps(data, ensure_ascii=False, cls=MyEncoder).encode('utf8')
+    return app.response_class(json_str, mimetype="application/json")
 
 
 @app.route("/")
 def hello_world():
-    return "<p>harrison lanny peace & rich</p>"
+    return "<p>孙正harrison lanny peace & rich</p>"
+
+
+@app.route("/stock/<symbol>")
+def stock(symbol):
+    # return f'获取 {symbol}对应的所有日线数据(从数据库)'
+    symbol = escape(symbol)
+    result = read_table("stock_basic", filter_str=f"WHERE `symbol`='{symbol}'", result_type='dict')
+    return _json(result)
 
 
 @app.route("/d/<symbol>")
 def d(symbol):
     # return f'获取 {symbol}对应的所有日线数据(从数据库)'
+    symbol = escape(symbol)
     table_name = f"d_{symbol}"
     return read_table(table_name, result_type='dict')
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     # show the user profile for that user
-#     return f'User {escape(username)}'
 
 
 # @app.route('/post/<int:post_id>')
@@ -32,3 +68,7 @@ def d(symbol):
 # def show_subpath(subpath):
 #     # show the subpath after /path/
 #     return f'Subpath {escape(subpath)}'
+
+if __name__ == '__main__':
+    # app.config['JSON_AS_ASCII'] = False
+    app.run(port=5555)
