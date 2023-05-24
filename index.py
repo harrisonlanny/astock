e16c8f8c2eed03eebe66046ec7bb714b42fae5e5
harrison <851596652@qq.com>
2023-05-24 23:05:38 +0800

    1

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from service.index import refresh_table_stock_basic, create_new_d_tables, delete
     update_d_tables
 
 
-refresh_table_stock_basic()
-create_new_d_tables()
+# refresh_table_stock_basic()
+# create_new_d_tables()
 update_d_tables(sleep_time=0.2, update_st=False)
 print('成功！！')
